033c8ddace88c3f52498a757d9cdfc29cf0a8b0c
Ryan Newman <ryannewman2828@gmail.com>
2017-11-13 23:29:42 -0500

    Init Backend

---
+++ Pathways-Project/server/api/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  // Temp values
+  res.json([{
+    id: 1,
+    username: 'samsepi0l',
+  }, {
+    id: 2,
+    username: 'D0loresH4ze',
+  }]);
+});
+
+
+module.exports = router;
---
+++ Pathways-Project/server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const routes = require('./api/routes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+});
+
+const port = process.env.PORT || 9000;
+const env = process.env.NODE_ENV || 'development';
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
+
+module.exports = app;
