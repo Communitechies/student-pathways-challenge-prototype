e40fb983182e04ba51d288dc933841746e65466c
Tyler Zhang <TZTylerzhang@gmail.com>
2017-11-24 23:11:49 -0500

    paths

--- Pathways-Project/src/App.js
+++ Pathways-Project/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router'
+import { Route, Switch } from 'react-router'
 import { ConnectedRouter } from 'react-router-redux'
 import GrommetApp from 'grommet/components/App';
 import Split from 'grommet/components/Split'
@@ -8,6 +8,7 @@ import Split from 'grommet/components/Split'
 import store, { history } from './store'
 
 import SideBar from './components/Sidebar'
+import MyPath from './components/MyPath'
 
 import './App.css';
 import 'grommet/grommet.min.css';
@@ -16,10 +17,13 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-          <GrommetApp>
+          <GrommetApp centered={false}>
             <ConnectedRouter history={history}>
               <Split fixed seperator>
                 <SideBar/>
+                <Switch>
+                  <Route path='/mypath' component={MyPath}/>
+                </Switch>
               </Split>
             </ConnectedRouter>
           </GrommetApp>
