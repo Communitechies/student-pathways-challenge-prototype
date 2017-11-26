36af7b124f7c71547856a67bf54b78c98e0ef4a0
Ryan Newman <ryannewman2828@gmail.com>
2017-11-25 21:49:37 -0500

    Sample pilot pathway

---
+++ Pathways-Project/server/pathways/pilot.js
@@ -0,0 +1,108 @@
+export default [
+  {
+    9: {
+      required: [
+        'ENG1D or ENG1P',
+      ],
+    },
+    10: {
+      required: [
+        'ENG2D',
+      ],
+    },
+    11: {
+      required: [
+        'ENG3U',
+      ],
+    },
+    12: {
+      required: [
+        {
+          course: 'ENG4U',
+          average: '70%',
+        },
+      ],
+    },
+    SE: [
+      {
+        programName: 'Geography and Aviation',
+        institution: 'University',
+        university: 'University of Waterloo',
+        description: 'Do you dream of being a pilot? Take to the skies with the largest university-level aviation program in Canada',
+        tuition: '$3,210/term',
+        average: '80-83',
+        ProgramType: 'Reg/Int/Prac',
+      },
+    ],
+    J: {
+      salaryRange: 'I dont know yet',
+      description: 'A pilot is someone who is in the aviation industry, and who is able to operate aircraft in order to transport passengers or goods from one location to another. They are employed by commercial airlines, corporations, or governments.',
+      otherJobs: [
+        'avionics mechanics',
+        'flight engineers',
+        'flying instructors',
+      ],
+    },
+  },
+  {
+    9: {
+      required: [
+        'ENG1D or ENG1P',
+        'SNC1D or SNC1P',
+      ],
+    },
+    10: {
+      required: [
+        'ENG2D',
+        'SNC2D',
+        'MPM2D',
+      ],
+    },
+    11: {
+      required: [
+        'ENG3U',
+        'MCR3U',
+        '1 of SBI3U, SCH3U, MCR3U or SPH3U',
+      ],
+    },
+    12: {
+      required: [
+        {
+          course: 'ENG4U',
+          average: '70%',
+        },
+        {
+          course: 'MHF4U',
+          average: '70%',
+        },
+        {
+          course: 'MCV4U',
+          average: '70%',
+        },
+        {
+          course: '2 of SBI4U, SCH4U, SES4U, MDM4U, or SPH4U',
+        },
+      ],
+    },
+    SE: [
+      {
+        programName: 'Science and Aviation',
+        institution: 'University',
+        university: 'University of Waterloo',
+        description: 'Earn a Bachelor of Science degree and your Commercial Pilot Licence through the largest aviation program in Canada.',
+        tuition: '$3,210/term',
+        average: '80-83',
+        ProgramType: 'Reg/Int/Prac',
+      },
+    ],
+    J: {
+      salaryRange: 'I dont know yet',
+      description: 'A pilot is someone who is in the aviation industry, and who is able to operate aircraft in order to transport passengers or goods from one location to another. They are employed by commercial airlines, corporations, or governments.',
+      otherJobs: [
+        'avionics mechanics',
+        'flight engineers',
+        'flying instructors',
+      ],
+    },
+  },
+];
