62ffdc263e96d612a5bfa8e926c96a34418ecb49
Daivik Goel <daivikgoel98@gmail.com>
2017-11-28 18:34:41 -0500

    Nursing Pathway

---
+++ Pathways-Project/server/pathways/nursing.js
@@ -0,0 +1,94 @@
+export default [
+    {
+        9: {
+            required: [
+                'ENG1D or ENG1P',
+                'SNC1D or SNC1P',
+                'MPM1D or MPM1P'
+            ],
+        },
+        10: {
+            required: [
+                'ENG2D',
+                'SNC2D',
+                'MPM2D',
+                'SNC2D'
+            ],
+        },
+        11: {
+            required: [
+                'ENG3U',
+                '1 of MCV4U or MHF4U',
+                'SCH3U',
+                '1 of MCR3U or MCT4C or MHF4U',
+                'SBI3U'
+
+            ],
+        },
+        12: {
+            required: [
+                {
+                    course: 'ENG4U',
+                    average: '65%',
+                },
+                {
+                    course: 'SBI4U',
+                    average: '65%',
+                },
+                {
+                    course: 'SCH4U',
+                    average: '65%',
+                },
+                {
+                    course: 'MHF4U',
+                    average: '65%',
+                },
+                {
+                    course: 'MCF3M',
+                    average: '65%',
+                },
+                {
+                    course'MCR3U'
+                    average: '65%',
+                },
+            ],
+        },
+        SE: [
+
+            {
+                programName: 'Nursing',
+                institution: 'University',
+                university: 'University of Western Ontario',
+                description: 'Develop clinical skills and confidence in a lab setting in one of our state-of-the-art simulation suites before embarking on your first practical placement.',
+                tuition: '$4,033.60/term',
+                average: '85%',
+                ProgramType: 'Reg/Int/Prac',
+            },
+            {
+                programName: 'Nursing',
+                institution: 'University',
+                university: 'Brock University',
+                description: 'Brock University’s nursing program is known for its rigorous curriculum, knowledgeable faculty and demanding standards.',
+                tuition: '$6,377.65term',
+                average: '87%-89%',
+                ProgramType: 'Reg/Int/Prac',
+            },
+            {
+                programName: 'Nursing',
+                institution: 'University',
+                university: 'University of Windsor',
+                description: 'The Faculty of Nursing provides many opportunities for you to gain valuable, hands-on experience that will ensure you receive a well-rounded education founded both in academic theory and real-world application.',
+                tuition: '$3,143.60/term',
+                average: '85',
+                ProgramType: 'Reg/Int/Prac',
+            },
+        ],
+        J: {
+            salaryRange: '$65,000 – $75,000',
+            description: 'Nurses provide and coordinate patient care, educate patients and the public about various health conditions, and provide advice and emotional support to patients and their family members.',
+            otherJobs: [
+
+            ],
+        },
+    }
+    ];
