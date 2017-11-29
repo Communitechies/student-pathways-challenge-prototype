c0e5d7f59ee4313bda47eeb10e6f93b7ca5247e1
Daivik Goel <daivikgoel98@gmail.com>
2017-11-28 19:13:58 -0500

    Teaching

---
+++ Pathways-Project/server/pathways/teacher.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        9: {
+            required: [ 
+            ],
+        },
+        10: {
+            required: [
+            ],
+        },
+        11: {
+            required: [
+
+            ],
+        },
+        12: {
+            required: [
+            ],
+        },
+        SE: [
+            {
+                programName: 'Teaching',
+                institution: 'University',
+                university: 'Trent University',
+                description: 'Trent Universitys School of Education and Professional Learning is an exciting place to teach and learn through our Consecutive B.Ed.Program, the new Indigenous Bachelor of Education program, the Teacher Education Stream, the Emphasis in Teacher Education Program, Professional Learning opportunities for in -service teachers, and our Masters in Educational Studies Program. ',
+                tuition: '$3823.41/term',
+                average: '75',
+                ProgramType: 'Reg/Int/Prac',
+            },
+            {
+                programName: 'Teaching',
+                institution: 'University',
+                university: 'University of Windsor',
+                description: 'Our mission is shaped by a commitment to equity and social justice, and at Windsor we offer specialized programming to enrich your experience.',
+                tuition: '$3,935.58/term',
+                average: '75-80',
+                ProgramType: 'Reg/Int/Prac',
+            },
+            {
+                programName: 'Teaching',
+                institution: 'University',
+                university: 'Lakehead University',
+                description: 'Lakehead Universitys Department of Undergraduate Studies in Education offers unique learning opportunities that help you to gain the knowledge, skills, attitudes, and qualifications to teach in Ontario or in other exciting locations.',
+                tuition: '$3,134.41/term',
+                average: '70',
+                ProgramType: 'Reg',
+            },
+
+
+        ],
+        J: {
+            salaryRange: '$36,190-$85,550',
+            description: 'Undergraduate Studies in Education involves the study and practice of teaching and works to prepare you to be a effective educator.',
+            otherJobs: [
+                'Instructor',
+                'High School Teacher',
+                'Middle School Teacher',
+                'Elementary School Teacher'
+
+
+            ],
+        },
+
+    },
+
+];
