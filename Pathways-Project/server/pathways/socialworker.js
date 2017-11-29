9cfc0e40d0c52f9c729dbbbd018c5c3d05791c07
Daivik Goel <daivikgoel98@gmail.com>
2017-11-28 21:46:27 -0500

    Added Social Worker

---
+++ Pathways-Project/server/pathways/socialworker.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        11: {
+            required: [
+            ],
+        },
+        12: {
+            required: [
+            ],
+        },
+        SE: [
+            {
+                programName: 'Social Work',
+                institution: 'University',
+                university: 'Lakehead University',
+                description: 'Our faculty is experienced and enthusiastic bringing a range of practice and research experience to the classroom. The result is a program that is truly exceptional. ',
+                tuition: '$3,134.60/term',
+                average: '70%',
+                ProgramType: 'Reg',
+            },
+            {
+                programName: 'Social Work',
+                institution: 'University',
+                university: 'Brock University',
+                description: 'A Social Sciences degree from Brock prepares you for a range of career options, and nurtures visionaries who will have great impact on advancing humanity and making the world a better place.',
+                tuition: '$3,284.60/term',
+                average: '74%-76%',
+                ProgramType: 'Reg',
+            },
+            {
+                programName: 'Social Work',
+                institution: 'University',
+                university: 'Carleton University',
+                description: 'Our faculty and some 2000 alumni are leaders in the development of a structural approach to social work practice that is rooted in social justice and social change. ',
+                tuition: '$3,896.91/term',
+                average: '75%',
+                ProgramType: 'Reg/Int/Prac',
+            },
+           
+        ],
+        J: {
+            salaryRange: '$48,448-$61,381',
+            description: 'Social work is an academic discipline and profession that concerns itself with individuals, families, groups and communities in an effort to enhance social functioning and overall well-being.',
+            otherJobs: [
+                'Counsellors',
+                'Therapists'
+                'Clergy'
+            ],
+        },
+    }
+];
